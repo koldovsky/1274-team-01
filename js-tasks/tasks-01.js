d668d3266409a728d9eac6e61d3b0730f72f32dd
AndreyDot <55506162+AndreyDot@users.noreply.github.com>
2024-09-20 16:48:15 +0300

    Add links on codeWars tasks in tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -4,34 +4,34 @@ function sayHello(name) {
   return "Hello, " + name;
 }
 
-//Messi goals function:
+//https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
-//Return Negative:
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
   if (num < 0) return num;
   else return -num;
 }
 
-//Terminal game move function:
+//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 function move(position, roll) {
   return position + roll * 2;
 }
 
-//Personalized Message
+//https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 function greet(name, owner) {
   if (name === owner) return "Hello boss";
   else return "Hello guest";
 }
 
-//Keep Hydrated!
+//https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 function litres(time) {
   return Math.floor(time * 0.5);
 }
 
-//Opposites Attract
+//https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 function lovefunc(flower1, flower2) {
   return flower1 % 2 !== flower2 % 2;
 }
