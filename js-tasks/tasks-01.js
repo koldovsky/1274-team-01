92376e0da168db445478f5f2a5eea1fa0bec2b24
AndreyDot <55506162+AndreyDot@users.noreply.github.com>
2024-09-20 16:44:29 +0300

    Added a task from codeWars.

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -3,3 +3,35 @@
 function sayHello(name) {
   return "Hello, " + name;
 }
+
+//Messi goals function:
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+//Return Negative:
+function makeNegative(num) {
+  if (num < 0) return num;
+  else return -num;
+}
+
+//Terminal game move function:
+function move(position, roll) {
+  return position + roll * 2;
+}
+
+//Personalized Message
+function greet(name, owner) {
+  if (name === owner) return "Hello boss";
+  else return "Hello guest";
+}
+
+//Keep Hydrated!
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+//Opposites Attract
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
+}
