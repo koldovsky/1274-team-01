3b6a9479ba93c32298c7f85c5b1ad8fe6fca54f2
Vladyslav <vladyslavdrushliak@gmail.com>
2024-09-26 22:31:52 +0300

    add phone number validation

--- js/contacts.collaborate-registration.js
+++ js/contacts.collaborate-registration.js
@@ -1,4 +1,30 @@
-// document - document object model (DOC)
+const phoneInput = document.querySelector('input[type="tel"]');
+const form = document.querySelector('.collaborate__registration');
 
-const mainTitle = document.querySelector('h2');
-mainTitle.innerText = 'texttexttexttexttexttexttext';
+const iti = window.intlTelInput(
+    phoneInput, {
+    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js",
+    initialCountry: "ua",
+    onlyCountries: ['us', 'gb', 'ua'],
+    separateDialCode: true
+});
+
+form.addEventListener('submit', function(event) {
+
+    const phoneValue = phoneInput.value.trim();
+
+    if (!/^\+?[0-9]*$/.test(phoneValue)) {
+        event.preventDefault();
+        alert(`заборонені символи в номері, ${phoneInput.value}!`);
+        return;
+    }
+
+    const isValidNumber = iti.isValidNumber(); // не перевіряє на зайві символи
+
+    if (!isValidNumber) {
+        event.preventDefault();
+        alert(`не валідний номер, ${phoneInput.value}!`);
+    }
+    alert(phoneInput.value);
+});
+
