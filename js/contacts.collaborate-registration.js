f174d24c25bf5494dd779d001a12cb3f874c4074
Vladyslav <vladyslavdrushliak@gmail.com>
2024-09-27 15:16:07 +0300

    Redo the form validation. Make part of the phone number validation

--- js/contacts.collaborate-registration.js
+++ js/contacts.collaborate-registration.js
@@ -1,30 +1,41 @@
-const phoneInput = document.querySelector('input[type="tel"]');
 const form = document.querySelector('.collaborate__registration');
 
-const iti = window.intlTelInput(
-    phoneInput, {
-    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js",
-    initialCountry: "ua",
-    onlyCountries: ['us', 'gb', 'ua'],
-    separateDialCode: true
-});
+const inputPhone = document.querySelector('input[name="phone"]');
+const maxLength = 10;
 
-form.addEventListener('submit', function(event) {
+const inputEmail = document.querySelector('input[name="email"]');
 
-    const phoneValue = phoneInput.value.trim();
+const buttonContactUs = document.querySelector('button[type="submit"]');
 
-    if (!/^\+?[0-9]*$/.test(phoneValue)) {
+
+inputPhone.addEventListener('keydown', (event) => {
+    const allowedKeys = ['Backspace', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
+
+    if (!allowedKeys.includes(event.key) &&
+        (event.key < '0' || event.key > '9')) {
         event.preventDefault();
-        alert(`заборонені символи в номері, ${phoneInput.value}!`);
-        return;
     }
 
-    const isValidNumber = iti.isValidNumber(); // не перевіряє на зайві символи
-
-    if (!isValidNumber) {
+    if (inputPhone.value.length >= maxLength &&
+        !allowedKeys.includes(event.key)) {
         event.preventDefault();
-        alert(`не валідний номер, ${phoneInput.value}!`);
     }
-    alert(phoneInput.value);
 });
 
+inputPhone.addEventListener('input', () => {
+    if (/^\d{1,10}$/.test(inputPhone.value) && inputPhone.value.length === maxLength) {
+        inputPhone.classList.add('valid');
+        inputPhone.classList.remove('invalid');
+    } else {
+        inputPhone.classList.remove('valid');
+        inputPhone.classList.add('invalid');
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    const isValid = /^\d{1,10}$/.test(inputPhone.value);
+    
+    if (!isValid || inputPhone.value.length !== maxLength) {
+        event.preventDefault();
+    }
+});
