693f4edef50963baba95e7324a5cddeb0e23abea
Andrii Dulenchuk <andrii.dulenchuk@gmail.com>
2024-10-06 00:02:31 +0000

    added looping animation for events list with js, changed comment in html file

--- js/about-us-section-our-services.js
+++ js/about-us-section-our-services.js
@@ -1,35 +1,37 @@
 const root = document.documentElement;
-const accentColor = getComputedStyle(root).getPropertyValue('--accent-text-color');
+const accentColor = getComputedStyle(root).getPropertyValue(
+  "--accent-text-color"
+);
 
-const accordionButtons = document.querySelectorAll('.our-services__accordion-item-button');
+const accordionButtons = document.querySelectorAll(
+  ".our-services__accordion-item-button"
+);
 
-accordionButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        accordionButtons.forEach(button => button.style.color = '');
-        const isExpanded = this.getAttribute('aria-expanded') === 'true';
-        if (isExpanded) {
-            button.style.color = accentColor;
-        }
-    })
-});
-
-const historyItem = document.querySelector('.our-services__company-history-item');
-
-
-
-/*
-// Select all service cards
-const serviceCards = document.querySelectorAll('.service-card');
-
-// Add hover effect for each card
-serviceCards.forEach(card => {
-  card.addEventListener('mouseenter', () => {
-    card.style.transform = 'scale(1.05)';
-    card.style.transition = 'transform 0.3s ease';
-  });
-
-  card.addEventListener('mouseleave', () => {
-    card.style.transform = 'scale(1)';
+accordionButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    accordionButtons.forEach((button) => (button.style.color = ""));
+    const isExpanded = this.getAttribute("aria-expanded") === "true";
+    if (isExpanded) {
+      button.style.color = accentColor;
+    }
   });
 });
-*/
+
+const historyItems = document.querySelectorAll(
+  ".our-services__company-history-item"
+);
+let i = 0;
+const intervalDuration = 1000; // Duration for each animation (1 second in this case)
+
+function animateItems() {
+  historyItems.forEach((item) => {
+    item.style.transform = "scale(1)";
+  });
+  const currentItem = historyItems[i % historyItems.length];
+  currentItem.style.transform = "scale(1.05)";
+  currentItem.style.transition = "transform 0.5s ease";
+  i++;
+}
+
+// Set interval to run the animation repeatedly
+setInterval(animateItems, intervalDuration);
