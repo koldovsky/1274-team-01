f60187725fce2193be52e0412be12f4bd9c42a43
Oleh Hanhal <hanhaloleg59@gmail.com>
2024-10-08 17:07:25 +0300

    uod css and js for index.section-image-gallery

--- js/index.section-image-gallery.partial.js
+++ js/index.section-image-gallery.partial.js
@@ -1,16 +1,34 @@
+// export function reduceOpacity() {
+//     const img_gal = document.querySelector('.image-gallery');
+//     img_gal.addEventListener('mouseover', function() {
+//         const text_header = this.querySelector('.image-gallery-text-header')
+//         text.classList.add('active');
+//         const text_caption = this.querySelector('.image-gallery-text-caption')
+//         text.classList.add('active');
+//     });
+
+//     img_gal.addEventListener('mouseout', function() {
+//         const text_header = this.querySelector('.image-gallery-text-header')
+//         text.classList.remove('active');
+//         const text_caption = this.querySelector('.image-gallery-text-caption')
+//         text.classList.remove('active');
+//     });
+// };
+
+
 export function reduceOpacity() {
     const img_gal = document.querySelector('.image-gallery');
     img_gal.addEventListener('mouseover', function() {
-        const text_header = this.querySelector('.image-gallery-text-header')
-        text.classList.add('active');
-        const text_caption = this.querySelector('.image-gallery-text-caption')
-        text.classList.add('active');
+        const text_header = this.querySelector('.hover-image-text-header');
+        text_header.classList.add('active');
+        const text_caption = this.querySelector('.hover-image-text-caption');
+        text_caption.classList.add('active');
     });
 
     img_gal.addEventListener('mouseout', function() {
-        const text_header = this.querySelector('.image-gallery-text-header')
-        text.classList.remove('active');
-        const text_caption = this.querySelector('.image-gallery-text-caption')
-        text.classList.remove('active');
+        const text_header = this.querySelector('.hover-image-text-header');
+        text_header.classList.remove('active');
+        const text_caption = this.querySelector('.hover-image-text-caption');
+        text_caption.classList.remove('active');
     });
-};
+}
